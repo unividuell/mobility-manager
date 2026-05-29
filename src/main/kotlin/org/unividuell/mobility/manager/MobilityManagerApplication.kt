e2b6b618ee560003ef37fb2608ee136b4c0796be
Clemens Hahn <clemens@couchbits.com>
2026-05-29 08:06:18 +0200

    prject setup by start.spring.io

---
+++ src/main/kotlin/org/unividuell/mobility/manager/MobilityManagerApplication.kt
@@ -0,0 +1,11 @@
+package org.unividuell.mobility.manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MobilityManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MobilityManagerApplication>(*args)
+}
---
+++ src/test/kotlin/org/unividuell/mobility/manager/MobilityManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.unividuell.mobility.manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MobilityManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
